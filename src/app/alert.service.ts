2fd114fc35b5a0fcca88f1a102d5004a887f3e25
Fabián Macías Esparza <al294996@edu.uaa.mx>
2025-04-21 00:40:38 -0600

    Lootie-web y Sweetalert
    Instalación de librerías

---
+++ src/app/alert.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor() { }
+
+  success(message: string, title: string = 'Éxito') {
+    Swal.fire(title, message, 'success');
+  }
+
+  error(message: string, title: string = 'Error') {
+    Swal.fire(title, message, 'error');
+  }
+
+  info(message: string, title: string = 'Información') {
+    Swal.fire(title, message, 'info');
+  }
+
+  warning(message: string, title: string = 'Advertencia') {
+    Swal.fire(title, message, 'warning');
+  }
+
+  confirm(message: string, title: string = '¿Estás seguro?') {
+    return Swal.fire({
+      title,
+      text: message,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'Cancelar'
+    });
+  }
+  
+  toast(message: string, title: string = 'Éxito', icon: 'success' | 'error' | 'warning' | 'info' = 'success') {
+    Swal.fire({
+      toast: true,
+      position: 'top-start',
+      showCloseButton: true,
+      icon: icon,
+      title,
+      text: message,
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+      }
+    });
+  }
+
+  
+}
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app/app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { MarkdownModule } from 'ngx-markdown';
+import { provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web/build/player/lottie_svg';
 
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(AppRoutingModule, BrowserAnimationsModule, HttpClientModule)],
+  providers: [importProvidersFrom(AppRoutingModule, BrowserAnimationsModule, HttpClientModule),
+    importProvidersFrom(MarkdownModule.forRoot()),
+    provideLottieOptions({
+      player: () => player,
+    }),
+  ],
 }).catch((err) => console.error(err));
