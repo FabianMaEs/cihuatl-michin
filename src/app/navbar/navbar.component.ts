e36df5899d88a79f66d9b66cb6c6543a2b8993d8
Fabián Macías Esparza <al294996@edu.uaa.mx>
2025-04-21 00:41:16 -0600

    Corrección de estilos

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,55 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterModule, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  animations: [
+    trigger('hamburguerX', [
+      /*
+        state hamburguer => is the regular 3 lines style.
+        states topX, hide, and bottomX => used to style the X element
+      */
+      state('hamburguer', style({})),
+      // style top bar to create the X
+      state(
+        'topX',
+        style({
+          transform: 'rotate(45deg)',
+          transformOrigin: 'left',
+          margin: '6px',
+        })
+      ),
+      // hides element when create the X (used in the middle bar)
+      state(
+        'hide',
+        style({
+          opacity: 0,
+        })
+      ),
+      // style bottom bar to create the X
+      state(
+        'bottomX',
+        style({
+          transform: 'rotate(-45deg)',
+          transformOrigin: 'left',
+          margin: '6px',
+        })
+      ),
+      transition('* => *', [
+        animate('0.2s'), // controls animation speed
+      ]),
+    ]),
+  ],
 })
 export class NavbarComponent {
-
+  isHamburguer = true;
+  onClick() {
+    this.isHamburguer = !this.isHamburguer;
+  }
 }
