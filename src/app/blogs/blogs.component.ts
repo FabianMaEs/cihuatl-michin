aad86efe684fc55cd6d73a3589903871264cf6df
Fabián Macías Esparza <al294996@edu.uaa.mx>
2025-04-21 02:17:18 -0600

    Corregido por encademiento opcional

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -130,10 +130,10 @@ export class BlogsComponent {
     this.markdown = this.blogSeleccionado ? this.blogSeleccionado.content : '';
     log('Blog seleccionado: ', this.blogSeleccionado);
     const blogSeleccionado = this.blogSeleccionado;
-    const blog = blogSeleccionado && blogSeleccionado._id 
+    const blog = blogSeleccionado?._id 
       ? this.blogs.find(b => b._id === blogSeleccionado._id) 
       : null;
-      
+
     if (blog) {
       blog.views = this.blogSeleccionado?.views ?? blog.views;
       blog.likes = this.blogSeleccionado?.likes ?? blog.likes;
